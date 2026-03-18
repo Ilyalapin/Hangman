5319350500280e5aa2f8e88ee140b753e02da3e6
Ilya Lapin <lap1nilia@yandex.ru>
2026-03-18 12:42:59 +0300

    добавил бесконечный цикл для работы игры

--- src/Starter.java
+++ src/Starter.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class Starter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Service game = new Service();
+
+        while (true) {
+            System.out.println("Введите" + "'" + "с" + "'" + ",чтобы начать игру или введите" + "'" + "в" + "'" + ",чтобы выйти из приложения.");
+            String userInput = scanner.nextLine();
+            if (Validator.isValid(userInput)) {
+                game.determineBehavior(userInput);
+            }
+        }
+    }
 }
