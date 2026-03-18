cd60ee589eec1a26510f0f8b753128328bb34509
Ilya Lapin <lap1nilia@yandex.ru>
2026-03-18 12:18:18 +0300

    написал метод isValid для Validator

--- src/Word.java
+++ src/Word.java
@@ -1,3 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Word {
 
+    private final Random random = new Random();
+
+    public String chooseRandomWord() {
+
+        List<String> words = new ArrayList<>();
+        words.add("юла");
+        words.add("окно");
+        words.add("кошка");
+        words.add("собака");
+        words.add("колбаса");
+        words.add("ласточка");
+
+        int randomIndex = random.nextInt(words.size());
+        return words.get(randomIndex);
+    }
 }
