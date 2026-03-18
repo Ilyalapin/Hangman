5505a6fae8e39b6c3ac44449da69dd27f48178b3
Ilya Lapin <lap1nilia@yandex.ru>
2026-03-18 12:34:32 +0300

    добавил метод decipherLetters

--- src/Service.java
+++ src/Service.java
@@ -3,4 +3,13 @@ public class Service {
     private static final int MAX_ERRORS = 6;
     private String secretWord;
     private String secretWordForConsole;
+
+    private void decipherLetters(String letter) {
+        for (int i = 0; i < secretWord.length(); i++) {
+            if (letter.indexOf(secretWord.charAt(i)) != -1) {
+                secretWordForConsole = secretWordForConsole.substring(0, i) +
+                        secretWord.charAt(i) + secretWordForConsole.substring(i + 1);
+            }
+        }
+    }
 }
