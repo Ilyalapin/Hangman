ed7104706ef99b633e3b93bbbd7e72d4deaa726b
Ilya Lapin <lap1nilia@yandex.ru>
2026-03-18 12:14:27 +0300

    написал метод isValid для Validator

--- src/Validator.java
+++ src/Validator.java
@@ -3,7 +3,12 @@ import java.util.regex.Pattern;
 public class Validator {
 
     public static boolean isValid(String letter) {
-
+        if (!Pattern.matches("^[а-я]$", letter)) {
+            System.out.println("****************************************************");
+            System.out.println("Необходимо ввести только одну строчную русскую букву");
+            System.out.println("****************************************************");
+            return false;
+        }
         return true;
     }
 }
