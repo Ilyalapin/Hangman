cd4cc00f087c7ad365c5f55c69c96d73c2f9852c
Ilya Lapin <lap1nilia@yandex.ru>
2026-03-18 12:34:57 +0300

    добавил метод printLossMessage

--- src/Service.java
+++ src/Service.java
@@ -4,6 +4,14 @@ public class Service {
     private String secretWord;
     private String secretWordForConsole;
 
+    private void printLossMessage() {
+        System.out.println(Scaffold.SIX);
+        System.out.println("***************************************************");
+        System.out.println("Вы допустили слишком много  ошибок. GAME OVER!!!" + "\n"
+                + "Загаданное слово: " + secretWord);
+        System.out.println("***************************************************");
+    }
+
     private void decipherLetters(String letter) {
         for (int i = 0; i < secretWord.length(); i++) {
             if (letter.indexOf(secretWord.charAt(i)) != -1) {
